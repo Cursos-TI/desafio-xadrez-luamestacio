793302db77c096de7835c60b96b782fc52f3af7c
luamestacio <202508435871@alunos.estacio.br>
2025-09-24 01:08:28 +0000

    Finalizado o desafio Aventureiro, eu acabei fazer 3 loops aninhados 1 for um while e um while for

--- xadrez.c
+++ xadrez.c
@@ -8,6 +8,9 @@ int main()
     int torre;      // Variável de controle para o movimento da Torre.
     int rainha = 0; // Variável de controle para o movimento da Rainha.
     int bispo = 0;  // Variável de controle para o movimento do Bispo.
+    int cavalow = 0; // Move-se duas casas para baixo e uma casa para esquerda
+    int cavalowh = 0; // Move-se duas casas para baixo e uma casa para esquerda
+
 
     // --- Movimento da Torre ---
     // Simula o movimento da Torre por 5 casas para a direita usando um loop 'for'.
@@ -35,5 +38,52 @@ int main()
         bispo++;
     }
  //Fim do desafio nivel novato
+
+     //Desafio aventureiro
+    //---- For aninhado inicio ----
+    /*
+    printf("Cavalo FOR \n");
+    for (int i = 0; i < 1; i++)
+    {
+        for (int passo_vertical = 0; passo_vertical < 2; passo_vertical++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
+*/ 
+    //---- For aninhado fim ----
+
+    //---- While aninhado inicio ----
+   /*     printf("Cavalo WHILE\n");
+
+        while (cavalow < 1)
+        {   
+            while (cavalowh < 2)
+        {
+            printf("Baixo\n");
+            cavalowh++;
+        }
+        printf("Esquerda\n");
+        cavalow++;
+        } 
+        
+     */   
+    //---- While aninhado fim ----
+
+    //--- While por fora e for por dentro inicio ---
+        printf("Cavalo While for\n");
+        while (cavalow < 1)
+        {
+        for (cavalowh = 0; cavalowh < 2; cavalowh++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+        cavalow++;
+        } 
+
+ //--- While por fora e for por dentro fim ---
+
     return 0;
 }
